f69cfe9f146674a7ac56f67fbf1c771176e9dac7
Unknown <fail.lee.smith@gmail.com>
2017-09-20 12:04:54 +0300

    Rational with some methods

--- sermiagina_e_p/rational/main.cpp
+++ sermiagina_e_p/rational/main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <sstream>
+//#include <stdexcept>
 
+
+using namespace std;
 
 class Rational {
 
 public:
+
     Rational() {}
     Rational (const int complete): _complete(complete){}
+
     Rational(const int complete, const int natural)
             : _complete(complete),
               _natural(natural)
     {
         if (_natural == 0) {
 
-            //throw error
+           throw invalid_argument("Zero in natural");
         }
-
-        //нормализация
+        normalize();
+        commonDivivsor(_complete, _natural);
     }
+
+    //operators
+    Rational& operator+=(const Rational& number);
+    Rational& operator-=(const Rational& number);
+    Rational& operator/=(const Rational& number);
+    Rational& operator*=(const Rational& number);
+
+    bool isPositive();
+
     std::ostream& writeTo(std::ostream& ostrm) const;
-    std::istream& readFrom(std::istream& istrm);
+    //std::istream& readFrom(std::istream& istrm);
 
 
 private:
@@ -29,15 +43,67 @@ private:
     ~Rational() = default;
 
 //реализация нормализации
+   int commonDivivsor(int firstNumber, int secondNumber) {
 
+        while (firstNumber && secondNumber) {
+            if (firstNumber > secondNumber) {
+                firstNumber = firstNumber % secondNumber;
+            } else {
+                secondNumber = secondNumber % firstNumber;
+            }
 
+        }
+        return (firstNumber + secondNumber);
+    }
 
-
+    void normalize();
 
 
 };
 
 
+Rational& Rational::operator/=(const Rational &number) {
+
+    _complete *= number._natural;
+    _natural *= number._complete;
+    normalize();
+
+    return *this;
+}
+
+
+Rational& Rational::operator*=(const Rational &number) {
+    _complete *= number._complete;
+    _natural *= number._natural;
+    normalize();
+
+    return *this;
+}
+
+Rational& Rational::operator+=(const Rational &number) {
+
+
+}
+
+
+void Rational::normalize() {
+    int nod = commonDivivsor(_complete, _natural);
+    _complete/=nod;
+    _natural/=nod;
+}
+
+
+
+bool Rational::isPositive() {
+    return (_natural > 0) && (_complete > 0);
+}
+
+
+//operators
+Rational operator+(const Rational& firstNumber, const Rational& secondNumber);
+Rational operator-(const Rational& firstNumber, const Rational& secondNumber);
+Rational operator/(const Rational& firstNumber, const Rational& secondNumber);
+Rational operator*(const Rational& firstNumber, const Rational& secondNumber);
 
 
 
